cdd3873622ecbc4490dd01bd16d30c1fe496977f
zheng <1324652616@qq.com>
2022-08-08 09:12:11 +0800

    fix

--- public/assets/js/backend/items_cate.js
+++ public/assets/js/backend/items_cate.js
@@ -32,12 +32,59 @@ define(['jquery', 'bootstrap', 'backend', 'table', 'form'], function ($, undefin
                             }
                         },
                         {field: 'label', title: __('Label'), operate: 'LIKE'},
+
+                        {field: 'id', title: '展开', operate: false, formatter: Controller.api.formatter.subnode},
                         {field: 'create_time', visible:false, title: __('Create_time'), operate:'RANGE', addclass:'datetimerange', autocomplete:false, formatter: Table.api.formatter.datetime},
                         {field: 'update_time', visible:false, title: __('Update_time'), operate:'RANGE', addclass:'datetimerange', autocomplete:false, formatter: Table.api.formatter.datetime},
                         {field: 'operate', title: __('Operate'), table: table, events: Table.api.events.operate, formatter: Table.api.formatter.operate}
                     ]
                 ],
                 pagination: false,
+            });
+
+
+            table.on('post-body.bs.table', function (e, settings, json, xhr) {
+                //默认隐藏所有子节点
+                $("a.btn[data-id][data-pid][data-pid!=0]").closest("tr").hide();
+                // $(".btn-node-sub.disabled").closest("tr").hide();
+                //显示隐藏子节点
+                $(".btn-node-sub").off("click").on("click", function (e) {
+                    var status = $(this).data("shown") ? true : false;
+                    $("a.btn[data-pid='" + $(this).data("id") + "']").each(function () {
+                        $(this).closest("tr").toggle(!status);
+                    });
+                    $(this).data("shown", !status);
+                    return false;
+                });
+                //点击切换/排序/删除操作后刷新左侧菜单
+                $(".btn-change[data-id],.btn-delone,.btn-dragsort").data("success", function (data, ret) {
+                    Fast.api.refreshmenu();
+                    return false;
+                });
+
+            });
+            //批量删除后的回调
+            $(".toolbar > .btn-del,.toolbar .btn-more~ul>li>a").data("success", function (e) {
+                Fast.api.refreshmenu();
+            });
+            //展开隐藏一级
+            $(document.body).on("click", ".btn-toggle", function (e) {
+                $("a.btn[data-id][data-pid][data-pid!=0].disabled").closest("tr").hide();
+                var that = this;
+                var show = $("i", that).hasClass("fa-chevron-down");
+                $("i", that).toggleClass("fa-chevron-down", !show);
+                $("i", that).toggleClass("fa-chevron-up", show);
+                $("a.btn[data-id][data-pid][data-pid!=0]").not('.disabled').closest("tr").toggle(show);
+                $(".btn-node-sub[data-pid=0]").data("shown", show);
+            });
+            //展开隐藏全部
+            $(document.body).on("click", ".btn-toggle-all", function (e) {
+                var that = this;
+                var show = $("i", that).hasClass("fa-plus");
+                $("i", that).toggleClass("fa-plus", !show);
+                $("i", that).toggleClass("fa-minus", show);
+                $(".btn-node-sub.disabled").closest("tr").toggle(show);
+                $(".btn-node-sub").data("shown", show);
             });
 
             // 为表格绑定事件
@@ -50,9 +97,16 @@ define(['jquery', 'bootstrap', 'backend', 'table', 'form'], function ($, undefin
             Controller.api.bindevent();
         },
         api: {
+            formatter: {
+                subnode: function (value, row, index) {
+                    return '<a href="javascript:;" data-toggle="tooltip" title="' + __('Toggle sub menu') + '" data-id="' + row.id + '" data-pid="' + row.pid + '" class="btn btn-xs '
+                        + (row.haschild == 1 || row.ismenu == 1 ? 'btn-success' : 'btn-default disabled') + ' btn-node-sub"><i class="fa fa-sitemap"></i></a>';
+                }
+            },
             bindevent: function () {
                 Form.api.bindevent($("form[role=form]"));
             }
+
         }
     };
     return Controller;
