ca6ca504bd66662c08769a59345ef6da1e0dc3cc
zheng <1324652616@qq.com>
2022-08-15 11:19:54 +0800

    fix download

--- application/extra/addons.php
+++ application/extra/addons.php
@@ -2,24 +2,7 @@
 
 return [
     'autoload' => false,
-    'hooks' => [
-        'config_init' => [
-            'alisms',
-            'qcloudsms',
-        ],
-        'sms_send' => [
-            'alisms',
-            'qcloudsms',
-        ],
-        'sms_notice' => [
-            'alisms',
-            'qcloudsms',
-        ],
-        'sms_check' => [
-            'alisms',
-            'qcloudsms',
-        ],
-    ],
+    'hooks' => [],
     'route' => [],
     'priority' => [],
     'domain' => '',
