723c9cd3b8daf57e533c9fbcda6a76b834ec6f6a
dingzhen <1324652616@qq.com>
2022-09-15 23:10:50 +0800

    fix

--- application/extra/addons.php
+++ application/extra/addons.php
@@ -3,6 +3,15 @@
 return [
     'autoload' => false,
     'hooks' => [
+        'sms_send' => [
+            'alisms',
+        ],
+        'sms_notice' => [
+            'alisms',
+        ],
+        'sms_check' => [
+            'alisms',
+        ],
         'upgrade' => [
             'simditor',
         ],
