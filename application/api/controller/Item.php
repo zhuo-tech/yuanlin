aa6c687887227daba4a90d827fa3849b69c0dc31
dingzhenznen <1324652616@qq.com>
2022-10-07 11:18:50 +0800

    fix

--- application/api/controller/Item.php
+++ application/api/controller/Item.php
@@ -40,7 +40,7 @@ class Item extends Api {
         $uid     = $request->param('uid', 0);
         $keyword = $request->param('keyword', '');
 
-        $data = ItemService::cate(['cid' => $cid, 'uid' => $uid, 'keyword' => $keyword], $page);
+        $data = ItemService::cate(['cid' => $cid, 'uid' => $uid, 'keyword' => $keyword], $page,$size);
         return json(['code' => 0, 'message' => 'OK', 'data' => $data]);
     }
 
