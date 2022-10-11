aa1b6629448dba566a935cf3df93ceee9293a59e
zheng <1324652616@qq.com>
2022-10-11 09:44:01 +0800

    fix

--- application/api/controller/User.php
+++ application/api/controller/User.php
@@ -83,9 +83,9 @@ class User extends Api {
         if (!Validate::regex($mobile, "^1\d{10}$")) {
             $this->error(__('Mobile is incorrect'));
         }
-        //        if (!Sms::check($mobile, $captcha, 'mobilelogin')) {
-        //            $this->error(__('Captcha is incorrect'));
-        //        }
+        if (!Sms::check($mobile, $captcha, 'mobilelogin')) {
+            $this->error(__('Captcha is incorrect'));
+        }
         $user = \app\common\model\User::getByMobile($mobile);
         if ($user) {
             if ($user->status != 'normal') {
