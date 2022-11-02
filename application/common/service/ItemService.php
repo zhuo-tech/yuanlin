b35a05d9f22d74e6ec52b197deff8aa3e80cf98e
dingzhenznen <1324652616@qq.com>
2022-11-02 22:07:38 +0800

    fix

--- application/common/service/ItemService.php
+++ application/common/service/ItemService.php
@@ -238,7 +238,7 @@ class ItemService {
 
         foreach ($item as &$v) {
             $images = explode(',',$v['images']);
-            $v['images']      =  ImagesService::getBaseUrl() . $images[0];
+            $v['images']      =  ImagesService::handleOut($images[0]);
             //$v['images'] =  ImagesService::getBaseUrl() . $v['images'];
         }
 
