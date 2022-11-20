f7c2d149452e1b6a27547a86833ef528095e914c
zheng <1324652616@qq.com>
2022-11-20 09:59:43 +0800

    fix

--- README.md
+++ README.md
@@ -8,6 +8,7 @@ copy .env.test 内容到.env
 1 安装 宝塔  端口权限
 2 安装lnmp
 3 配置mysql 访问授权
+4 安装git (拉取代码)
 4 配置网站 nginx
 5 配置php权限函数
 4 安装fastadmin插件
--- public/NnxHwCFjem.php
+++ public/NnxHwCFjem.php
@@ -16,10 +16,10 @@
 define('APP_PATH', __DIR__ . '/../application/');
 
 // 判断是否安装
-if (!is_file(APP_PATH . 'admin/command/Install/install.lock')) {
-    header("location:./install.php");
-    exit;
-}
+//if (!is_file(APP_PATH . 'admin/command/Install/install.lock')) {
+//    header("location:./install.php");
+//    exit;
+//}
 
 // 加载框架引导文件
 require __DIR__ . '/../thinkphp/base.php';
